f0e538a24a0a4bd1f05bcd277d7ff3a8b75d93c1
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-08-21 15:18:05 -0700

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,10 @@
-# Enter your procedural solution here!
+def largest_prime_factor(input)
+    prime = input
+    (2..Math.sqrt(input).to_i).each do |i|
+      break if prime <= 1
+      while (prime > i && prime % i == 0)
+        prime /= i
+      end
+    end
+    prime
+  end
